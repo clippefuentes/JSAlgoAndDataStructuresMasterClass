2a0192fce17b8b754315763201bcc90b9ab6d88b
clippefuentes <clippefuentes@gmail.com>
2021-11-14 17:02:49 -0700

    add binary tree

---
+++ BinaryTree.js
@@ -0,0 +1,55 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+    insert(val) {
+        const newNode = new Node(val)
+        if (!this.root) {
+            this.root = newNode
+            return this
+        } else {
+            let current = this.root 
+            while(true) {
+                if (value === current.value) return undefined
+                if (val < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode
+                        return this
+                    } else {
+                        current = current.left
+                    }
+                } else if (val > current.val) {
+                    if (current.right === null) {
+                        current.right = newNode
+                        return this
+                    } else {
+                        current = current.right
+                    }
+                }
+            }
+            
+        }
+    }
+    find(val) {
+        if(!this.root) return false
+        let current = this.root, found = false
+        while(!found && current) {
+            if (value < current.val) {
+                current = current.left
+            } else if (value > current.val) {
+                current = current.right
+            } else {
+                return true
+            }
+        }
+        return false        
+    }
+}
