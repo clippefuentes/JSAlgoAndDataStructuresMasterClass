4d4899256916a791e42b8b6a7f3a195554549c21
clippefuentes <clippefuentes@gmail.com>
2021-11-15 19:52:29 -0700

    Add DFS

--- DoubleLinkedList.js
+++ DoubleLinkedList.js
@@ -160,10 +160,43 @@ class DoubleLinkedList {
         queue.push(node)
         while (queue.length) {
             node = queue.shift()
-            data.push(node)
+            data.push(node.value)
             if (node.left) queue.push(node)
             if (node.right) queue.push(node.right)
         }
         return data
+    }
+
+    DFSPreOrder() {
+        const data = []
+        function traverse(node) {
+            data.push(node.value)
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+        }
+        traverse(this.root)
+        return data
+    }
+
+    DFSPostOrder() {
+        const data = []
+        function traverse(node) {
+            if (node.left) traverse(node.left)
+            if (node.right) traverse(node.right)
+            data.push(node.value)
+        }
+        traverse(this.root)
+        return data
+    }
+
+    DFSInOrder(){
+        const data = []
+        function traverse(node) {
+            if (node.left) traverse(node.left)
+            data.push(node.value)
+            if (node.right) traverse(node.right)
+        }
+        traverse(this.root)
+        return data 
     }
 }
